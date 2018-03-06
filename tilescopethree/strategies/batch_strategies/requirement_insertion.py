4601d14432cb2f5e0ac8569b75175f4123a7b43f
Tomas Ken <tomkenmag@gmail.com>
2018-03-06 15:35:18 +0000

    Implemented requirement insertion

--- tilescopethree/strategies/batch_strategies/requirement_insertion.py
+++ tilescopethree/strategies/batch_strategies/requirement_insertion.py
@@ -1,81 +1,36 @@
+"""
+    Module containing the requirement insertion strategy.
+"""
 from comb_spec_searcher import Strategy
-from permuta import PermSet, Perm
-from grids_two import Obstruction, Requirement, Tiling
-
-def root_requirement_insertions(tiling, basis, maxreqlen=None, **kwargs):
-    if tiling.dimensions != (1, 1):
-        return
-    if tiling.requirements:
-        return
-    if maxreqlen is None:
-        maxreqlen = max(len(x) for x in basis)
-    if ((0, 0)) in tiling.possibly_empty:
-        for length in range(1, maxreqlen + 1):
-            for patt in PermSet.avoiding(basis).of_length(length):
-                yield Strategy(
-                    formal_step=(
-                        "Inserting requirement {} into cell {}").format(patt, (0, 0)),
-                    objects=[Tiling(tiling.point_cells,
-                                    tiling.positive_cells,
-                                    tiling.possibly_empty,
-                                    tiling.obstructions + (
-                                        Obstruction.single_cell(patt, (0, 0)),),
-                                    tiling.requirements),
-                             Tiling(tiling.point_cells,
-                                    tiling.positive_cells | {(0, 0)},  # Make cell
-                                    tiling.possibly_empty - {(0, 0)},  # positive
-                                    tiling.obstructions,
-                                    [[Requirement.single_cell(patt, (0, 0))]])],
-                    workable=[False, True])
+from grids_three import Obstruction, Tiling
 
 
-def all_one_by_one_requirement_insertions(tiling, basis, **kwargs):
-    if tiling.dimensions != (1, 1):
-        return
+def requirement_insertion(tiling, basis, **kwargs):
+    """
+    The requirement insertion strategy.
 
-    if tiling.requirements and (len(tiling.requirements) != 1 or
-                                len(tiling.requirements[0]) != 1):
-        return
+    The requirement insertion strategy is a batch strategy that considers each
+    requirement of each requirement list. For each of these requirements, the
+    strategy returns two tilings; one where the requirement has been turned
+    into an obstruction and another where the requirement has been singled out
+    and a new requirement list added with only the requirement. This new
+    requirement list contains only the singled out requirement.
 
-    if ((0, 0) not in tiling.positive_cells and
-            (0, 0) not in tiling.possibly_empty):
-        return
-
-    maxreqlen = kwargs.get('maxreqlen')
-    patterns = kwargs.get('patterns')
-    if maxreqlen is None:
-        maxreqlen = max(len(patt) for patt in basis)
-
-    if not tiling.requirements:
-        req = Requirement.single_cell(Perm(), (0, 0))
-    else:
-        req = tiling.requirements[0][0]
-
-    if not patterns and maxreqlen and len(req) >= maxreqlen:
-        return
-    elif patterns and not maxreqlen:
-        if not any(patt.contains(req.patt) for patt in patterns):
-            raise ValueError(("Requirement pattern {} not contained in any of "
-                             "the target patterns {}").format(req.patt,
-                                                              patterns))
-
-    for patt in PermSet.avoiding(basis).of_length(len(req) + 1):
-        if patt.avoids(req.patt):
+    This implements the notion of partitioning the set of gridded permutations
+    into those that satisfy this requirement and those that avoid it. Those
+    that avoid the requirement, must therefore satisfy another requirement from
+    the same list and hence the requirement list must be of length at least 2.
+    """
+    for reqs in tiling.requirements:
+        if len(reqs) == 1:
             continue
-        if patterns and not any(p.contains(patt) for p in patterns):
-            continue
-        yield Strategy(
-            formal_step=(
-                "Inserting requirement {} into cell {}").format(patt, (0, 0)),
-            objects=[Tiling(tiling.point_cells,
-                            tiling.positive_cells,
-                            tiling.possibly_empty,
-                            tiling.obstructions + (
-                                Obstruction.single_cell(patt, (0, 0)),),
-                            tiling.requirements),
-                     Tiling(tiling.point_cells,
-                            tiling.positive_cells | {(0, 0)},  # Make cell
-                            tiling.possibly_empty - {(0, 0)},  # positive
-                            tiling.obstructions,
-                            [[Requirement.single_cell(patt, (0, 0))]])],
-            workable=[False, True])
+        for req in reqs:
+            yield Strategy(
+                formal_step="",
+                objects=[
+                    Tiling(obstructions=tiling.obstructions,
+                           requirements=tiling.requirements + ((req,),)),
+                    Tiling(obstructions=tiling.obstructions + (
+                        Obstruction(req.patt, req.pos),),
+                           requirements=tiling.requirements)],
+                workable=[True, True])
