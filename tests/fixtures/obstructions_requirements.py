1c4ff7c7e7ad4517280318962200fa315e61969b
Tomas Ken <tomkenmag@gmail.com>
2018-03-07 16:20:46 +0000

    added tests for requirement_corroboration

---
+++ tests/fixtures/obstructions_requirements.py
@@ -0,0 +1,51 @@
+import pytest
+from grids_three import Obstruction, Requirement
+from permuta import Perm
+
+
+@pytest.fixture
+def typical_redundant_obstructions():
+    """Returns a very typical list of obstructions clustered together in a
+    corner of a tiling.  """
+    return [
+        Obstruction(Perm((0, 1)), ((1, 0), (1, 0))),
+        Obstruction(Perm((0, 1)), ((1, 0), (2, 0))),
+        Obstruction(Perm((0, 1)), ((1, 0), (3, 0))),
+        Obstruction(Perm((0, 1)), ((2, 0), (2, 0))),
+        Obstruction(Perm((0, 1)), ((2, 0), (3, 0))),
+        Obstruction(Perm((0, 1)), ((3, 1), (3, 1))),
+        Obstruction(Perm((1, 0)), ((3, 0), (3, 0))),
+        Obstruction(Perm((1, 0)), ((3, 1), (3, 0))),
+        Obstruction(Perm((1, 0)), ((3, 1), (3, 1))),
+        Obstruction(Perm((0, 1, 2)), ((3, 0), (3, 0), (3, 0))),
+        Obstruction(Perm((0, 1, 2)), ((3, 0), (3, 0), (3, 1))),
+        Obstruction(Perm((2, 1, 0)), ((1, 0), (1, 0), (1, 0))),
+        Obstruction(Perm((2, 1, 0)), ((1, 0), (1, 0), (2, 0))),
+        Obstruction(Perm((2, 1, 0)), ((1, 0), (1, 0), (3, 0))),
+        Obstruction(Perm((2, 1, 0)), ((1, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((2, 1, 0)), ((1, 0), (2, 0), (3, 0))),
+        Obstruction(Perm((2, 1, 0)), ((2, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((2, 1, 0)), ((2, 0), (2, 0), (3, 0))),
+        Obstruction(Perm((3, 2, 1, 0)), ((1, 1), (2, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((3, 2, 1, 0)), ((2, 1), (2, 1), (3, 0), (3, 0)))
+    ]
+
+
+@pytest.fixture
+def typical_redundant_requirements():
+    """Returns a very typical list of requirements of a tiling.  """
+    return [
+        [Requirement(Perm((0, 1, 2)), ((0, 0), (1, 0), (2, 3))),
+         Requirement(Perm((0, 1, 2)), ((0, 0), (1, 0), (2, 4))),
+         Requirement(Perm((1, 0, 2)), ((0, 0), (1, 0), (2, 3))),
+         Requirement(Perm((1, 0, 2)), ((0, 1), (1, 0), (2, 3)))],
+        [Requirement(Perm((0, 1, 2)), ((2, 3), (2, 3), (2, 3))),
+         Requirement(Perm((1, 0, 2)), ((0, 0), (0, 0), (0, 0))),
+         Requirement(Perm((0, 1, 2)), ((1, 0), (1, 0), (1, 0)))],
+        [Requirement(Perm((0, 1)), ((1, 0), (3, 0))),
+         Requirement(Perm((0, 1)), ((2, 0), (2, 0))),
+         Requirement(Perm((0, 1)), ((2, 0), (3, 0))),
+         Requirement(Perm((0, 1)), ((2, 0), (3, 1)))],
+        [Requirement(Perm((1, 0)), ((3, 3), (3, 1))),
+         Requirement(Perm((1, 0)), ((3, 1), (3, 1))),
+         Requirement(Perm((1, 0)), ((3, 1), (3, 0)))]]
