9d21d695ac2081383154bdce11e9c0ce9a6f16fd
Tomas Ken <tomkenmag@gmail.com>
2018-03-12 16:25:10 +0000

    tidied up globally_verified

--- tilescopethree/strategies/verification_strategies/globally_verified.py
+++ tilescopethree/strategies/verification_strategies/globally_verified.py
@@ -1,10 +1,14 @@
 from comb_spec_searcher import VerificationStrategy
 
+
 def globally_verified(tiling, **kwargs):
-    '''Can't handle requirements, so rage quit.'''
-    if tiling.requirements:
-        return
+    """The globally verified strategy.
+
+    A tiling is globally verified if every requirement and obstruction is
+    non-interleaving.
+    """
     if not tiling.dimensions == (1, 1):
         if all(not ob.is_interleaving() for ob in tiling.obstructions):
-            if all(all(not r.is_interleaving() for r in req) for req in tiling.requirements):
+            if all(all(not r.is_interleaving() for r in req)
+                   for req in tiling.requirements):
                 return VerificationStrategy(formal_step="Globally verified.")
