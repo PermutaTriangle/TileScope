c18ec55e66ca06fc26f50c0a06340c3f28fe8155
Tomas Ken <tomkenmag@gmail.com>
2018-03-12 15:58:27 +0000

    Removing trailing whitespace

--- tilescopethree/strategies/verification_strategies/is_empty.py
+++ tilescopethree/strategies/verification_strategies/is_empty.py
@@ -6,5 +6,5 @@ def is_empty_strategy(tiling, **kwargs):
     for reqlist in tiling.requirements:
         if all(any(ob in req for ob in tiling) for req in reqlist):
             return True
-            
+
     return False
