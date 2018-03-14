57ffb9a8ad70c0d9562f4a0992e053560407cc08
Tomas Ken <tomkenmag@gmail.com>
2018-03-14 15:36:04 +0000

    Added database verification to package

--- tilescopethree/strategies/verification_strategies/__init__.py
+++ tilescopethree/strategies/verification_strategies/__init__.py
@@ -1,7 +1,5 @@
 from .subset_verified import subset_verified
-
 from .is_empty import is_empty_strategy
-
 from .globally_verified import globally_verified
-
 from .miner_verification import miner_verified
+from .database_verification import database_verified
