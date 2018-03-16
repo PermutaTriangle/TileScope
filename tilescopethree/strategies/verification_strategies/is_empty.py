4d65a93dc7d2941eec5f66997b8b69fe90476e77
Tomas Ken <tomkenmag@gmail.com>
2018-03-16 13:08:28 +0000

    cleaned up is_empty_strategy

--- tilescopethree/strategies/verification_strategies/is_empty.py
+++ tilescopethree/strategies/verification_strategies/is_empty.py
@@ -1,13 +1,4 @@
 
 def is_empty_strategy(tiling, **kwargs):
-    """The is empty verification strategy.
-
-    A tiling is empty if during initialition of the object contradictory
-    obstructions and requirements are detected.
-    """
-    if tiling.is_empty():
-        return True
-    for reqlist in tiling.requirements:
-        assert not all(any(ob in req for ob in tiling.obstructions)
-                       for req in reqlist)
-    return False
+    """The is empty verification strategy."""
+    return tiling.is_empty()
