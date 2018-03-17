6a10068d900724a0b2dc7cb5c4b9dcc909e89e28
Tomas Ken <tomkenmag@gmail.com>
2018-03-17 12:46:49 +0000

    updated strategy packs

--- tilescopethree/strategy_packs.py
+++ tilescopethree/strategy_packs.py
@@ -1,15 +1,17 @@
 from comb_spec_searcher import StrategyPack
-from tilescopethree.strategies import (all_cell_insertions,
-                                       factor,
-                                       globally_verified, point_placement,
+from tilescopethree.strategies import (all_cell_insertions, database_verified,
+                                       empty_cell_inferral, factor,
+                                       globally_verified, is_empty_strategy,
+                                       point_placement,
                                        requirement_corroboration,
                                        row_and_column_separation,
                                        subset_verified)
 
-point_placement = StrategyPack(
+point_placement_only = StrategyPack(
          eq_strats=[point_placement],
-         ver_strats=[subset_verified, globally_verified],
-         inf_strats=[row_and_column_separation],
+         ver_strats=[subset_verified, globally_verified,
+                     database_verified, is_empty_strategy],
+         inf_strats=[row_and_column_separation, empty_cell_inferral],
          other_strats=[[factor], [all_cell_insertions],
                        [requirement_corroboration]],
          name="point_placement")
