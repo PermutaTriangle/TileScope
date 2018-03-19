d9d55d53603f3f4222446f023476bc94de36a90a
Tomas Ken <tomkenmag@gmail.com>
2018-03-19 17:34:12 +0000

    updated point placement strategy pack with obstruction_transitivity

--- tilescopethree/strategy_packs.py
+++ tilescopethree/strategy_packs.py
@@ -2,6 +2,7 @@ from comb_spec_searcher import StrategyPack
 from tilescopethree.strategies import (all_cell_insertions, database_verified,
                                        empty_cell_inferral, factor,
                                        globally_verified, is_empty_strategy,
+                                       obstruction_transitivity,
                                        point_placement,
                                        requirement_corroboration,
                                        row_and_column_separation,
@@ -11,7 +12,8 @@ point_placement_only = StrategyPack(
          eq_strats=[point_placement],
          ver_strats=[subset_verified, globally_verified,
                      database_verified, is_empty_strategy],
-         inf_strats=[row_and_column_separation, empty_cell_inferral],
+         inf_strats=[empty_cell_inferral, obstruction_transitivity,
+                     row_and_column_separation],
          other_strats=[[factor], [all_cell_insertions],
                        [requirement_corroboration]],
          name="point_placement")
