8a16adac157a1458660f45e82c6d10521499740a
Tomas Ken <tomkenmag@gmail.com>
2018-03-19 16:12:45 +0000

    obstruction_transitivity

--- tilescopethree/strategies/inferral_strategies/__init__.py
+++ tilescopethree/strategies/inferral_strategies/__init__.py
@@ -1,2 +1,3 @@
 from .subobstruction_inferral import empty_cell_inferral
 from .row_and_column_separation import row_and_column_separation
+from .obstruction_transitivity import obstruction_transitivity
