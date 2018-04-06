d437edf6cccb39981c2b796f93e3bbf422aae0a3
Christian Bean <christianbean@ru.is>
2018-04-06 11:13:47 +0000

    adding more fundamental packs

--- tilescopethree/strategy_packs.py
+++ tilescopethree/strategy_packs.py
@@ -84,6 +84,15 @@ length_2_requirement_with_pattern_placement = StrategyPack(
                        [requirement_corroboration]],
          name="length_2_requirement_with_pattern_placement")
 
+fundamental_length_2_requirement_with_point_placement = StrategyPack(
+         eq_strats=[],
+         ver_strats=[fundamentally_verified],
+         inf_strats=[empty_cell_inferral, obstruction_transitivity,
+                     row_and_column_separation],
+         other_strats=[[factor], [partial(all_cell_insertions, maxreqlen=2)],
+                       [requirement_placement], [requirement_corroboration]],
+         name="fundamental_length_2_requirement_with_point_placement")
+
 fundamental_length_2_requirement_with_pattern_placement = StrategyPack(
          eq_strats=[],
          ver_strats=[fundamentally_verified],
@@ -136,6 +145,15 @@ length_3_requirement_with_pattern_placement = StrategyPack(
                         partial(all_requirement_extensions, maxreqlen=3)],
                        [requirement_corroboration]],
          name="length_3_requirement_with_pattern_placement")
+
+fundamental_length_3_requirement_with_point_placement = StrategyPack(
+         eq_strats=[],
+         ver_strats=[fundamentally_verified],
+         inf_strats=[empty_cell_inferral, obstruction_transitivity,
+                     row_and_column_separation],
+         other_strats=[[factor], [partial(all_cell_insertions, maxreqlen=3)],
+                       [requirement_placement], [requirement_corroboration]],
+         name="fundamental_length_3_requirement_with_point_placement")
 
 fundamental_length_3_requirement_with_pattern_placement = StrategyPack(
          eq_strats=[],
