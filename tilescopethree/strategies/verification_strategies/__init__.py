c9d6718d50ea0f50e4d596320194297712881637
Christian Bean <christianbean@ru.is>
2018-04-29 20:10:46 +0000

    delete is_empty strategy imports

--- tilescopethree/strategies/verification_strategies/__init__.py
+++ tilescopethree/strategies/verification_strategies/__init__.py
@@ -1,5 +1,4 @@
 from .subset_verified import subset_verified
-from .is_empty import is_empty_strategy
 from .globally_verified import globally_verified, fundamentally_verified
 from .miner_verification import miner_verified
 from .database_verification import database_verified
