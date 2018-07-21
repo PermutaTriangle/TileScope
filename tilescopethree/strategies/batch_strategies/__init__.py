c59842904e1273a96839ab530f427fee80523600
Christian Bean <christianbean@ru.is>
2018-07-21 23:28:16 +0000

    root req packs

--- tilescopethree/strategies/batch_strategies/__init__.py
+++ tilescopethree/strategies/batch_strategies/__init__.py
@@ -1,4 +1,5 @@
 from .requirement_corroboration import requirement_corroboration
 from .cell_insertion import (all_cell_insertions, all_point_insertions,
-                             all_requirement_extensions)
+                             all_requirement_extensions,
+                             root_requirement_insertion)
 from .insertion_encoding import insertion_encoding
