b380f0d54e113673156b314814ffe62febe24ff7
Henning Ulfarsson <ulfarsson@gmail.com>
2018-10-01 16:36:41 +0000

    Adding verify short obs

---
+++ tilescopethree/strategies/verification_strategies/verify_short_obs.py
@@ -0,0 +1,13 @@
+"""A strategy for checking if a tiling only has obstructions of length at most two."""
+
+from comb_spec_searcher import VerificationStrategy
+
+def verify_short_obstructions(tiling, **kwargs):
+    """
+    A tiling is verified if it only has obstructions of length at most two.
+    """
+    if tiling.dimensions == (1, 1):
+        return None
+    else:
+        if all(len(obs)<3 for obs in tiling.obstructions):
+            return VerificationStrategy(formal_step=("The tiling only has obstructions of length at most two."))
