a914e06c0f82b137356b25213823e4e27bc72381
Émile Nadeau <nadeau.emile@gmail.com>
2019-09-06 16:44:48 +0000

    row and col rule now on tiling

--- tilescopethree/strategies/inferral_strategies/row_and_column_separation.py
+++ tilescopethree/strategies/inferral_strategies/row_and_column_separation.py
@@ -6,7 +6,4 @@ from tilings.algorithms import RowColSeparation
 
 def row_and_column_separation(tiling, **kwargs):
     rcs = RowColSeparation(tiling)
-    if rcs.separable():
-        formal_step = 'Row and column separation'
-        separated_tiling = rcs.separated_tiling()
-        return InferralRule(formal_step, separated_tiling)
+    return rcs.rule()
