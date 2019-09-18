769091a133b0f240506367cdd0295f018eea0641
Émile Nadeau <nadeau.emile@gmail.com>
2019-09-18 12:53:04 +0000

    new test for searcher

--- tests/test_tilescope.py
+++ tests/test_tilescope.py
@@ -3,8 +3,10 @@ import sympy
 
 from comb_spec_searcher import ProofTree
 from tilescopethree import TileScopeTHREE
-from tilescopethree.strategy_packs_v2 import (point_placements,
-                                              point_placements_fusion)
+from tilescopethree.strategy_packs_v2 import (
+    point_placements, point_placements_fusion,
+    point_placements_fusion_with_interleaving,
+    row_and_col_placements_fusion_with_interleaving_fusion)
 
 
 @pytest.mark.timeout(20)
@@ -22,4 +24,33 @@ def test_132():
 def test_123():
     searcher = TileScopeTHREE('123', point_placements_fusion)
     t = searcher.auto_search(smallest=True)
+    assert isinstance(t, ProofTree)
+
+
+@pytest.mark.timeout(20)
+def test_1342_1423():
+    searcher = TileScopeTHREE('1342_1423',
+                              point_placements_fusion_with_interleaving)
+    t = searcher.auto_search(smallest=True)
+    t.number_of_nodes() == 14
+    assert isinstance(t, ProofTree)
+
+
+@pytest.mark.timeout(20)
+def test_1324():
+    searcher = TileScopeTHREE(
+        '1324',
+        row_and_col_placements_fusion_with_interleaving_fusion
+    )
+    t = searcher.auto_search(smallest=True)
+    t.number_of_nodes() == 14
+    num_fusion = 0
+    num_comp_fusion = 0
+    for node in t.nodes():
+        if 'Fuse' in node.formal_step:
+            num_fusion += 1
+        if 'Component' in node.formal_step:
+            num_comp_fusion += 1
+    assert num_fusion == 1
+    assert num_comp_fusion == 1
     assert isinstance(t, ProofTree)
