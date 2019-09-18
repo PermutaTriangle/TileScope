c54e496e8dbdc43c2343070c4a220950f1b809d6
Émile Nadeau <nadeau.emile@gmail.com>
2019-09-18 12:58:14 +0000

    test for fusion_with_interleaving strategy

---
+++ tests/strategies/equivalence_strategies/test_fusion_with_interleaving.py
@@ -0,0 +1,41 @@
+import pytest
+
+from permuta import Perm
+from tilescopethree.strategies import fusion_with_interleaving
+from tilings import Obstruction, Tiling
+
+
+@pytest.fixture
+def tiling1():
+    t = Tiling(obstructions=[
+        Obstruction(Perm((1, 0)), ((0, 1), (1, 1))),
+        Obstruction(Perm((1, 0)), ((0, 1), (0, 1))),
+        Obstruction(Perm((1, 0)), ((0, 1), (1, 0))),
+        Obstruction(Perm((1, 0)), ((0, 1), (0, 0))),
+        Obstruction(Perm((1, 0)), ((0, 0), (0, 0))),
+        Obstruction(Perm((1, 0)), ((0, 0), (1, 0))),
+        Obstruction(Perm((1, 0)), ((1, 0), (1, 0))),
+        Obstruction(Perm((1, 0)), ((1, 1), (1, 0))),
+        Obstruction(Perm((1, 0)), ((1, 1), (1, 1)))
+    ])
+    return t
+
+
+@pytest.fixture
+def tiling2():
+    t = Tiling(obstructions=[
+        Obstruction(Perm((0, 1)), ((0, 0), (1, 0))),
+        Obstruction(Perm((0, 2, 1)), ((0, 0), (0, 0), (0, 0))),
+        Obstruction(Perm((0, 2, 1)), ((1, 0), (1, 0), (1, 0))),
+        Obstruction(Perm((0, 2, 1, 3)), ((0, 0), (0, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((0, 2, 1, 3)), ((0, 0), (2, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((0, 2, 1, 3)), ((1, 0), (1, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((0, 2, 1, 3)), ((1, 0), (2, 0), (2, 0), (2, 0))),
+        Obstruction(Perm((0, 2, 1, 3)), ((2, 0), (2, 0), (2, 0), (2, 0)))
+    ])
+    return t
+
+
+def test_fusion_with_interleaving(tiling1, tiling2):
+    assert len(list(fusion_with_interleaving(tiling1))) == 0
+    assert len(list(fusion_with_interleaving(tiling2))) == 1
