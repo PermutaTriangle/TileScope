831c4586ccc767fca678e17d4acc0ad08b968c57
Émile Nadeau <nadeau.emile@gmail.com>
2020-02-06 09:52:55 +0000

    migrate batch strategies

---
+++ tilescopethree/strategies/batch_strategies.py
@@ -0,0 +1,13 @@
+from tilings.strategies.batch import (all_cell_insertions, all_col_insertions,
+                                      all_factor_insertions,
+                                      all_point_insertions,
+                                      all_requirement_extensions,
+                                      all_requirement_insertions,
+                                      all_row_insertions, col_placements,
+                                      partial_col_placements,
+                                      partial_row_and_col_placements,
+                                      partial_row_placements,
+                                      requirement_corroboration,
+                                      requirement_list_placement,
+                                      root_requirement_insertion,
+                                      row_and_col_placements, row_placements)
