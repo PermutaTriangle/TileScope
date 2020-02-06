20e49084282f905fb342963fa3c0038dc74a8689
Émile Nadeau <nadeau.emile@gmail.com>
2020-02-06 10:01:24 +0000

    fusion strategies have moved in tilings

--- tilescopethree/strategies/equivalence_strategies.py
+++ tilescopethree/strategies/equivalence_strategies.py
@@ -1,5 +1,6 @@
-from tilings.strategies.equivalence import (all_placements, component_fusion,
-                                            fusion, partial_point_placement,
+from tilings.strategies.equivalence import (all_placements,
+                                            partial_point_placement,
                                             partial_requirement_placement,
                                             point_placement,
                                             requirement_placement)
+from tilings.strategies.fusion import component_fusion, fusion
